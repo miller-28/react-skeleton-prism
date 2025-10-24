edb9e1fb0cb2c4f405485a12129a4f1a9c39301d
miller <jonathan@miller28.com>
2025-10-24 16:44:55 +0200

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./styles/app.css";
-import SiteLayout from "./components/SiteLayout.jsx";
+import Layout from "./components/Layout.jsx";
 
 export default function App() {
   return (
     <>
-        <SiteLayout />
+        <Layout />
     </>
   );
 }
